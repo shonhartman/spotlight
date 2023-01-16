482d5a94333b10dcf6e21ffd4fb992d3ac423a4d
shonhartman <shaun@bighuman.com>
2023-01-15 20:50:21 -0500

    first blog

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
+          Shaun Hartman - Software engineer, musician, digital creator
         </title>
         <meta
           name="description"
@@ -261,13 +261,12 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+          Shaun Hartman - Digital Creator
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Shaun, a software engineer at Big Human and a life long creator based in Little Rock.<br></br>
+            I like to rock.
+            I try to teach whenever called upon & learn whenever fortunate.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
